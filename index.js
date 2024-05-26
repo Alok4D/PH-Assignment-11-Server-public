c7bb1cdbce680f09c9bb6b05e4765aa7b5e96205
Alok4D <alokroy602701@gmail.com>
2024-05-26 19:21:02 +0600

    add new server side code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1yjndj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,8 +29,60 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const foodCollection = client.db('foodDB').collection('food')
+    const foodCollection = client.db('foodDB').collection('food');
+    const foodPurchaseCollection = client.db('foodPurchaseDB').collection('foodPurchase');
+    const foodPhotoCollection = client.db('foodPhotoDB').collection('foodPhoto');
 
+
+
+    //foodPurchase ...
+    app.delete('/foodPurchase/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await foodPurchaseCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get("/foodPurchase", async (req, res) => {
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}}
+      const result = await foodPurchaseCollection.find(query).toArray();
+      res.send(result);
+  })
+
+    app.get('/foodPurchase', async(req, res) => {
+      const cursor = foodPurchaseCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/foodPurchase/:_id', async (req, res) => {
+        const updatedFood = req.body;
+        const id =  req.params._id;
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert : true}
+        const finalFood = {$set: {...updatedFood}}
+        const updatedResult = await foodCollection.updateOne(filter, finalFood, options)
+
+        const result = await foodPurchaseCollection.insertOne(updatedFood)
+        res.send({result, updatedResult});
+    })
+    //
+
+   //gallery ......
+    app.get('/foodPhoto', async(req, res) => {
+      const cursor = foodPhotoCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/foodPhoto', async (req, res) => {
+        const newAddFoodPhoto = req.body;
+        const result = await foodPhotoCollection.insertOne(newAddFoodPhoto)
+        res.send(result);
+    })
+    // 
     app.get('/food', async(req, res) => {
       const cursor = foodCollection.find();
       const result = await cursor.toArray();
@@ -38,7 +91,6 @@ async function run() {
 
     app.post('/food', async(req, res) => {
         const newFood = req.body;
-        console.log(newFood);
         const result = await foodCollection.insertOne(newFood)
         res.send(result);
     })
